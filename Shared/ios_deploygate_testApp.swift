dac0ab7967b1da09503a7fe4b8ac8886c611b32b
waarrk <washioyusaku@icloud.com>
2022-08-17 13:06:34 +0900

    Initial Commit

---
+++ Shared/ios_deploygate_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_deploygate_testApp.swift
+//  Shared
+//
+//  Created by Washio on 2022/08/17.
+//
+
+import SwiftUI
+
+@main
+struct ios_deploygate_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
